8e777d5e365a96675e06ba4eb31b64a9ae3c27e3
Sibints2000 <sibints2000@gmail.com>
2021-09-15 19:56:06 +0530

    Forieth Commit

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
 
-window.onscroll = () =>{
+window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
 
 document.querySelectorAll('.image-slider img').forEach(images => {
 
-    var src = images.getAttribute('src');
-    document.querySelector('.main-home-image').src = src;
+    images.onclick = () => {
+        var src = images.getAttribute('src');
+        document.querySelector('.main-home-image').src = src;
+    }
 
 });
