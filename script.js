14c38e7cbb2f25a89da138490e49bd599e36a3ef
Sibints2000 <sibints2000@gmail.com>
2021-09-15 23:11:53 +0530

    Ninety Seventh Commit

--- script.js
+++ script.js
@@ -31,17 +31,17 @@ var swiper = new Swiper(".review-slider", {
         disableOnInteraction: false,
     },
     breakpoints: {
-          640: {
+        640: {
             slidesPerView: 2,
             spaceBetween: 20,
-          },
-          768: {
+        },
+        768: {
             slidesPerView: 4,
             spaceBetween: 40,
-          },
-          1024: {
+        },
+        1024: {
             slidesPerView: 5,
             spaceBetween: 50,
-          },
         },
+    },
 });
